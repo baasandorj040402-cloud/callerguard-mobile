71541172cd31191b357b2cddb4c090f8ac98b6cd
Todkhuu <otoduu@gmail.com>
2026-01-16 17:33:37 +0800

    backend url change

--- app/src/main/java/com/erdene/callerinsight/Constants.kt
+++ app/src/main/java/com/erdene/callerinsight/Constants.kt
@@ -3,5 +3,5 @@ package com.erdene.callerinsight
 
 object Constants {
     // TODO: Replace with your actual backend URL
-    const val BACKEND_URL = "https://callerguard-backend.onrender.com/analyze"
+    const val BACKEND_URL = "https://104.197.162.88:8080/analyze"
 }
